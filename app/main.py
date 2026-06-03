b72afe36001770eaa2b015df7d48c4f4dae9f94d
Giddy-chordz <gideonakinpelumi@gmail.com>
2026-06-03 12:05:16 +0100

    cors set_up

--- app/main.py
+++ app/main.py
@@ -2,12 +2,25 @@ from fastapi import FastAPI
 from .database import engine, get_db
 from . import models, config
 from .routers import auth, products, carts
-
-
-app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
 
 #create tables defined in models.py in the database
 models.Base.metadata.create_all(bind = engine)
+
+app = FastAPI()
+
+#setup CORS middleware to allow requests from the frontend application running on localhost:3000
+origins = ["http://localhost:3000"]
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 
 #include the authentication router
 app.include_router(auth.router)
